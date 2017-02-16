97a471efa09c6acc94bab94b212bff64713d80ee
SarahNox <apnoeist@googlemail.com>
2017-02-16 20:32:57 +0100

    first ideas

---
+++ app.js
@@ -0,0 +1,101 @@
+const express            = require('express');
+const path               = require('path');
+const favicon            = require('serve-favicon');
+const logger             = require('morgan');
+const cookieParser       = require('cookie-parser');
+const bodyParser         = require('body-parser');
+const expressLayouts     = require('express-ejs-layouts');
+const passport           = require('passport');
+const LocalStrategy      = require('passport-local').Strategy;
+const User               = require('./models/user');
+const bcrypt             = require('bcrypt');
+const session            = require('express-session');
+const MongoStore         = require('connect-mongo')(session);
+const mongoose           = require('mongoose');
+const flash              = require('connect-flash');
+
+mongoose.connect('mongodb://localhost:27017/foodExchange');
+
+// var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(require('node-sass-middleware')({
+//   src: path.join(__dirname, 'public'),
+//   dest: path.join(__dirname, 'public'),
+//   indentedSyntax: false,
+//   sourceMap: true
+// }));
+app.use('/bower_components', express.static(path.join(__dirname, 'bower_components/')))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// app.use('/', index);
+app.use('/', users);
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findOne({ "_id": id }, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+passport.use(new LocalStrategy({passReqToCallback: true}, (req, username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+    return next(null, user);
+  });
+}));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(session({
+  secret: 'passport-local-strategy',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/users.js
@@ -0,0 +1,69 @@
+const express        = require("express");
+const router         = express.Router();
+// User model
+const User           = require("../models/user");
+// Bcrypt to encrypt passwords
+const bcrypt         = require("bcrypt");
+const bcryptSalt     = 10;
+const ensureLogin = require("connect-ensure-login");
+const passport      = require("passport");
+
+router.get("/private-page", ensureLogin.ensureLoggedIn(), (req, res) => {
+  res.render("users/private", { user: req.user });
+});
+
+router.get("/signup", (req, res, next) => {
+  res.render("users/signup");
+});
+
+router.get("/login", (req, res, next) => {
+  res.render("users/login");
+});
+
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "/private-page",
+  failureRedirect: "/login",
+  failureFlash: true,
+  passReqToCallback: true
+}));
+
+router.post("/signup", (req, res, next) => {
+  var username = req.body.username;
+  var password = req.body.password;
+
+  if (username === "" || password === "") {
+    res.render("users/signup", { message: "Indicate username and password" });
+    return;
+  }
+
+  User.findOne({ username }, "username", (err, user) => {
+    if (user !== null) {
+      res.render("users/signup", { message: "The username already exists" });
+      return;
+    }
+
+    var salt     = bcrypt.genSaltSync(bcryptSalt);
+    var hashPass = bcrypt.hashSync(password, salt);
+
+    var newUser = User({
+      username,
+      password: hashPass
+    });
+
+    newUser.save((err) => {
+      console.log(err);
+      if (err) {
+        res.render("users/signup", { message: "The username already exists" });
+      } else {
+        res.redirect("/login");
+      }
+    });
+  });
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/login");
+});
+
+module.exports = router;
