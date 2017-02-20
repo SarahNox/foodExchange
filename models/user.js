5b8e4ace309d4574867ff092fdbc3c1da76305d7
SarahNox <apnoeist@googlemail.com>
2017-02-20 14:46:34 +0100

    updating models and adding google marker

--- models/user.js
+++ models/user.js
@@ -3,12 +3,15 @@ const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
   username: String,
-  password: String
-  // address = {
-  //   street: String,
-  //   city: String,
-  //   postal_code: String
-  // }
+  password: String,
+  address: {
+    street: String,
+    number: String,
+    flat: String,
+    door: String,
+    city: String,
+    postal_code: String
+  },
 }, {
   timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
 });
