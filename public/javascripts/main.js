b6a7e0e0b0463e7dc5843e652bf438624637e916
SarahNox <apnoeist@googlemail.com>
2017-02-22 13:10:00 +0100

    delete uncommented code

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -88,19 +88,3 @@ $(document).ready(function(){
     }
   });
 });
-
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude
-      };
-      var circle = new google.maps.Circle({
-        center: geolocation,
-        radius: position.coords.accuracy
-      });
-      autocomplete.setBounds(circle.getBounds());
-    });
-  }
-}
